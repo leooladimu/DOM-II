34e1c4c5d7a26f093e41c052f0158c8a345b7071
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-08-31 22:34:49 -0400

    third

--- js/index.js
+++ js/index.js
@@ -1,18 +1,29 @@
 // Your code goes here
-// const btn = document.querySelector('.logo-heading');
-// console.log(btn);
-// function random(number) {
-//   return Math.floor(Math.random() * (number+1));
-// }
+const btn = document.querySelector('.logo-heading');
+console.log('I love you');
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
 
-// btn.onclick = function() {
-//     const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-//     document.body.style.backgroundColor = rndCol;
-//   }
+btn.onclick = function() {
+    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = rndCol;
+  }
 
-const pic = document.querySelector('.main-navigation');
-// console.log(pic);
+const pic = document.querySelector('.text-content');
+console.log(pic);
   
-pic.addEventListener("mouseover", function( event ) {
-    event.target.style.color = "red";
-});
+pic.addEventListener('mouseover', ( event ) =>
+    event.target.style.color = 'green');
+
+pic.addEventListener('mouseout', (event) =>
+    event.target.style.color = 'black');
+
+const tag = pic.nextElementSibling;
+    console.log(tag);
+    tag.addEventListener('mouseover', (event) => 
+    event.target.style.color = 'red');
+
+const pick = document.querySelector('. content-pick p');
+    console.log(pick);
+    window.addEventListener('load', (event) => event.target.style.textAlign = 'center');
