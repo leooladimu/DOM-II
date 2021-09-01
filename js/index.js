7f026dc2d0ea774f03ae6d3ae029627a693d32af
leooladimu <82811737+leooladimu@users.noreply.github.com>
2021-08-31 20:38:31 -0400

    one

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const btn = document.querySelector('.logo-heading');
+console.log(btn);
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+btn.onclick = function() {
+    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = rndCol;
+  }
+
+const pic = document.querySelector('.content-destination');
+// console.log(pic);
+  
+pic.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "red";
+});
